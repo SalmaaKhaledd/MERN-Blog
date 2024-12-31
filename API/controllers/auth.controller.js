1c42ef2bb4b079a766e3dba428f6d1c4e1add2e2
Salma Khaled <77202068+SalmaKhaled879@users.noreply.github.com>
2025-01-01 01:45:53 +0200

    create sig in api route

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import e from 'express';
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'; 
 import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup= async (req, res,next) => {
   const {username, email, password} = req.body;  //req body is in json format containing key value pairs of username, email and password
@@ -26,4 +27,35 @@ try {
   next(error); //to pass the error to the global error handling middleware
 }
 };
+
+export const signin= async (req, res,next) => {
+  const{email, password} = req.body;  
+  if(!email || !password || email === ' ' || password === ' '){
+    next(errorHandler(400,"All fields are required"));
+  }
+  try{
+    //check if the user exists in the database
+    const validUser= await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,"User not found"));
+    }
+    //compare the password entered by the user with the hashed password in the database
+    const validPassword= bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400,"Invalid password"));
+    }
+    const token=jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    //seperate the password from the rest of the user data
+    const {password: pass, ...rest}=validUser._doc;
+    //nobody can understand what the access token is (without the jwt secret key)except the server
+    res.status(200).cookie('access_token', token, {
+      httpOnly: true}).json(rest);
+    
+  }
   
+  catch(error){
+    next(error);
+  }
+
+
+};
--- API/routes/auth.route.js
+++ API/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {signup} from '../controllers/auth.controller.js';
+import {signin} from '../controllers/auth.controller.js';
 import e from 'express';
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
