a42c6e1001b199a80fe5fa1f311ed095cfb583e1
Salma Khaled <77202068+SalmaKhaled879@users.noreply.github.com>
2025-01-01 22:10:25 +0200

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'flowbite/dist/flowbite.css'
 import App from './App.jsx'
-import {store} from './redux/store.jsx'
+import {store, persistor} from './redux/store.jsx'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 
 createRoot(document.getElementById('root')).render(
   //provider is a component that makes the Redux store available to any nested components that need to access the Redux store
   
   <StrictMode>
+    <PersistGate persistor={persistor}> 
      <Provider store={store}> {/* makes the Redux store available to any nested components that need to access the Redux store */}
     <App />
     </Provider>
+    </PersistGate>
   </StrictMode>
   
 )
--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,11 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { get, version } from 'mongoose'
 
+
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistConfig = {
+  key:'root', 
+  storage,
+  version: 1,
+};
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
 
 //setting up global store object
 //registers any reducer functions that are passed to it, and makes the combined reducer available to the rest of the application
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({serializableCheck: false})
+});
+
+export const persistor = persistStore(store);
